f3000aeeb4dac41360948aab9fea60d28bc55f63
Nicholas H.Tollervey <ntoll@ntoll.org>
2023-04-04 12:54:03 +0100

    Make image clickable in loosey goosey example.

--- examples/loosey_goosey/main.py
+++ examples/loosey_goosey/main.py
@@ -1,17 +1,26 @@
 """
-The simplest possible app. It says, "Hello, World!".
+Honking as a service.
 """
 from pypercard import App, Card
 
 honk_app = App(
     name="Loosey Goosey.",
-    card_list=[Card("goose_card"),],
-    sounds={"honk": "honk.mp3", },
+    card_list=[
+        Card("goose_card"),
+    ],
+    sounds={
+        "honk": "honk.mp3",
+    },
 )
 
 
 @honk_app.transition("goose_card", "honk", "click")
-def honk(card, datastore):
+def honk_button(card, datastore):
+    card.play_sound("honk")
+
+
+@honk_app.transition("goose_card", "goose", "click")
+def honk_picture(card, datastore):
     card.play_sound("honk")
 
 
