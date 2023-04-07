dd6f18b72065f231c1071e19b6e55f4d808fc565
Nicholas H.Tollervey <ntollervey@anaconda.com>
2023-04-07 15:19:00 +0100

    Simpler Hello.

--- examples/hello/main.py
+++ examples/hello/main.py
@@ -3,11 +3,8 @@ The simplest possible app. It says, "Hello, World!".
 """
 from pypercard import App, Card
 
-# Create an app, called "Hello", with a single "hello" card.
+# Create an app, with a single "hello" card.
 app = App(
-    # The optional name of the app become's the page title, in the browser.
-    name="Hello",
-    # The optional card_list contains the initial stack of cards.
     card_list=[
         # The card is named "hello", and will display the template's content.
         Card(name="hello", template="Hello, world!"),
