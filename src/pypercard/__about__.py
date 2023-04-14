0a81e9f074df59de6c52559d330fd1e957a2eb48
Nicholas H.Tollervey <ntoll@ntoll.org>
2023-04-14 19:31:46 +0100

    Dynamic version resolution from the __version__ in __about__.py (as per standard practices).

--- src/pypercard/__about__.py
+++ src/pypercard/__about__.py
@@ -18,11 +18,4 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from .core import Card, App
-from .datastore import DataStore
-
-__all__ = [
-    "Card",
-    "App",
-    "DataStore",
-]
+__version__ = "0.0.1.alpha.6"
