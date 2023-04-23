01480f95c62071d46169bae02d6b0af84e3a1dcf
Fabio Pliger <fpliger@anaconda.com>
2023-04-23 17:04:09 -0600

    add dict to store all the hooks registered on a Pypercard App

--- src/pypercard/core.py
+++ src/pypercard/core.py
@@ -408,6 +408,8 @@ class App:
         style = document.createElement("style")
         style.innerText = "html, body {width:100%;height:100%;}"
         document.head.appendChild(style)
+        # Dict to collect handlers of event hooks, registered by users
+        self._registered_hooks = collections.defaultdict(list)
 
     def _new_id(self):
         """
