5ad417b34e5e2fb1739239878c42cf6963db63f7
Martin <martin.chilvers@gmail.com>
2024-02-21 13:38:36 -0600

    Gardening the farmyard example.

--- examples/farmyard/main.py
+++ examples/farmyard/main.py
@@ -1,70 +1,18 @@
 import invent
-from invent.ui import from_datastore
 
 
-# User interface.
-invent.ui.App(
-    name="Loosey Goosey",
-    content=[
-        invent.ui.Page(
-            name="Honk",
-            content=[
-                invent.ui.Image(
-                    invent.media.images.goose.png,
-                    channel="honk"
-                ),
-                invent.ui.Button(
-                    name="button honk",
-                    label="HONK!!!",
-                    channel="honk"
-                ),
-                invent.ui.Button(
-                    name="to_pig",
-                    label="Visit Percy",
-                    channel="navigate"
-                ),
-                invent.ui.TextBox(
-                    name="number_of_honks",
-                    text=from_datastore("number_of_honks"),
-                )
-            ],
-        ),
-        invent.ui.Page(
-            name="Oink",
-            content=[
-                invent.ui.Image(
-                    invent.media.images.pig.png,
-                    channel="oink"
-                ),
-                invent.ui.Button(
-                    name="button oink",
-                    label="OINK!",
-                    channel="oink"
-                ),
-                invent.ui.Button(
-                    name="to_goose",
-                    label="Visit Lucy",
-                    channel="navigate"
-                ),
-                invent.ui.TextBox(
-                    name="number_of_oinks",
-                    text=from_datastore("number_of_oinks"),
-                )
-            ],
-        )
-    ],
-)
+# Datastore ############################################################################
 
 
-# Datastore.
-invent.datastore.update(number_of_honks=0)
-if "number_of_oinks" not in invent.datastore:
-    invent.datastore.update(number_of_oinks=0)
+invent.datastore.setdefault("number_of_honks", 0)
+invent.datastore.setdefault("number_of_oinks", 0)
 
 
-# Code (stacks of blocks).
+# Code #################################################################################
+
+
 def make_honk(message):
-    invent.datastore["number_of_honks"] = invent.datastore["number_of_honks"] + 1 
+    invent.datastore["number_of_honks"] = invent.datastore["number_of_honks"] + 1
     invent.play_sound(invent.media.sounds.honk.mp3)
 
 
@@ -79,11 +27,72 @@ def move_page(message):
     elif message.button == "to_pig":
         invent.show_page("Oink")
 
-# Channels.
+
+# Channels #############################################################################
+
+
 invent.subscribe(make_honk, to_channel="honk", when_subject=["press", "touch"])
 invent.subscribe(make_oink, to_channel="oink", when_subject=["press", "touch"])
 invent.subscribe(move_page, to_channel="navigate", when_subject=["press",])
 
 
-# GO!
+# User Interface #######################################################################
+
+
+invent.ui.App(
+    name="Farmyard",
+    content=[
+        invent.ui.Page(
+            name="Honk",
+            content=[
+                invent.ui.Image(
+                    invent.media.images.goose.png,
+                    channel="honk"
+                ),
+                invent.ui.Button(
+                    name="button honk",
+                    label="HONK!",
+                    channel="honk"
+                ),
+                invent.ui.Button(
+                    name="to_pig",
+                    label="Visit Percy",
+                    channel="navigate"
+                ),
+                invent.ui.TextBox(
+                    name="number_of_honks",
+                    text=invent.ui.from_datastore("number_of_honks"),
+                )
+            ],
+        ),
+        invent.ui.Page(
+            name="Oink",
+            content=[
+                invent.ui.Image(
+                    invent.media.images.pig.png,
+                    channel="oink"
+                ),
+                invent.ui.Button(
+                    name="button oink",
+                    label="OINK!!",
+                    channel="oink"
+                ),
+                invent.ui.Button(
+                    name="to_goose",
+                    label="Visit Lucy",
+                    channel="navigate"
+                ),
+                invent.ui.TextBox(
+                    name="number_of_oinks",
+                    text=invent.ui.from_datastore("number_of_oinks"),
+                )
+            ],
+        )
+    ],
+)
+
+
+# GO! ##################################################################################
+
+
 invent.go()
