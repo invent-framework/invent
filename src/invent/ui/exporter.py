9d3376b24171d12efcc80a2c0d265f02a321b7b3
Martin <martin.chilvers@gmail.com>
2024-03-01 13:50:35 -0600

    Always put content last in the list of properties.

--- src/invent/ui/exporter.py
+++ src/invent/ui/exporter.py
@@ -34,51 +34,57 @@ def _pretty_repr_lines(component, lines=None, indent=""):
 
     lines = lines or []
 
-    #### e.g. "Page("
-    line = f"{indent}{type(component).__name__}("
-
     component_properties = type(component).properties()
     component_is_container = "content" in component_properties
 
     if component_is_container:
         content_from_datastore = getattr(component, "_content_from_datastore", None)
 
+    #### The component class e.g. "Page("
+    lines.append(f"{indent}{type(component).__name__}(")
+
+    indent += "    "
+
+    #### The component's properties (other than it's content if it is a container.
     for i, (property_name, property_obj) in enumerate(component_properties.items()):
-        if property_name == "content" and not content_from_datastore:
+        # Put the content last in the list of properties...
+        if property_name == "content":
             continue
-
-        if property_name in ["id", "name"] and not property_obj:
-            continue
-
-        # if type(value) is str and not value:
-        #     continue
 
         from_datastore = getattr(component, property_obj.private_name + "_from_datastore", None)
         if from_datastore:
-            line += f"{property_name}=from_datastore('{from_datastore.key}'"
+            line = f"{indent}{property_name}=from_datastore('{from_datastore.key}'"
             if from_datastore.via_function:
                 line += f", via_function={from_datastore.via_function.__name__}"
             line += ")"
 
         else:
-            line += f"{property_name}={repr(getattr(component, property_name))}"
+            line = f"{indent}{property_name}={repr(getattr(component, property_name))}"
 
         if i < len(component_properties) + 1:
             line += ", "
 
-    if component_is_container and not content_from_datastore:
-        line += "content=["
         lines.append(line)
 
-        for child in component.content:
-            _pretty_repr_lines(child, lines, indent+"    ")
-            if child is not component.content[-1]:
+    if component_is_container:
+        if not content_from_datastore:
+            lines.append(f"{indent}content=[")
+
+            for child in component.content:
+                _pretty_repr_lines(child, lines, indent+"    ")
+                #if child is not component.content[-1]:
                 lines[-1] += ", "
 
-        lines.append("])")
+            lines.append(f"{indent}]")
 
-    else:
-        line = line + ")"
-        lines.append(line)
+        else:
+            line = f"{indent}content=from_datastore('{content_from_datastore.key}'"
+            if content_from_datastore.via_function:
+                line += f", via_function={content_from_datastore.via_function.__name__}"
+            line += ")"
+            lines.append(line)
+
+    indent = indent[:-4]
+    lines.append(f"{indent})")
 
     return lines
