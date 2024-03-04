857bb3bd3b426671fa9d1a325c122782539169ba
joshualowe1002 <joshualowe1002@gmail.com>
2024-03-04 19:29:44 +0000

    Change icon to preview

--- src/tools/builder/src/views/builder/components/widget-preview/widget-preview.vue
+++ src/tools/builder/src/views/builder/components/widget-preview/widget-preview.vue
@@ -2,7 +2,7 @@
 
 <template>
     <div class="p-4 bg-gray-100 rounded-md border text-center flex flex-col space-y-2 font-medium">
-        <div v-html="widget.properties.icon.default_value" class="text-3xl mx-auto" />
+        <div v-html="widget.preview" class="text-3xl mx-auto" />
         <span>{{ widget.name }}</span>
     </div>
 </template>
