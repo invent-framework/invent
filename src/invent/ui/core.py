c95aac5af3dda8dd5a812afebe05b98145c07fe8
Nicholas H.Tollervey <ntoll@ntoll.org>
2024-03-04 17:49:29 +0000

    Add default icon.

--- src/invent/ui/core.py
+++ src/invent/ui/core.py
@@ -38,6 +38,8 @@ _TSHIRT_SIZES = (
     "L",
     "XL",
 )
+#: The default icon for a component.
+_DEFAULT_ICON = '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256"><path fill="currentColor" d="M140 180a12 12 0 1 1-12-12a12 12 0 0 1 12 12M128 72c-22.06 0-40 16.15-40 36v4a8 8 0 0 0 16 0v-4c0-11 10.77-20 24-20s24 9 24 20s-10.77 20-24 20a8 8 0 0 0-8 8v8a8 8 0 0 0 16 0v-.72c18.24-3.35 32-17.9 32-35.28c0-19.85-17.94-36-40-36m104 56A104 104 0 1 1 128 24a104.11 104.11 0 0 1 104 104m-16 0a88 88 0 1 0-88 88a88.1 88.1 0 0 0 88-88"/></svg>'  # noqa
 
 
 class ValidationError(ValueError):
@@ -520,6 +522,10 @@ class Component:
     position = TextProperty(
         "The component's position inside it's parent.",
         default_value="FILL",
+    )
+    icon = TextProperty(
+        "The component's SVG icon for the graphical builder.",
+        default_value=_DEFAULT_ICON,
     )
 
     def __init__(self, **kwargs):
