3696cd40b5d73e6ad778f9bc52fe9ff1108da512
Martin <martin.chilvers@gmail.com>
2024-03-14 20:36:58 -0500

    Update overlords.py

--- examples/demo/overlords.py
+++ examples/demo/overlords.py
@@ -4,26 +4,10 @@ import requests
 pyodide_http.patch_all()
 
 
-def read_document(url):
-    """Read a text-based document from the specified URL."""
-
-    proxy_url = f"https://mchilvers.pyscriptapps.com/my/api-proxies/the-web"
-
-    response = requests.get(proxy_url, headers={"-url": url})
-    if response.status_code != 200:
-        raise ValueError("Can't read that URL")
-
-    return response.text()
-
-
-def ask_the_overlords(prompt, use_ai_launcher=True, ai_launcher_port=8765):
+def ask_the_overlords(prompt, ai_launcher_port=8765):
     """Ask the overlords!"""
 
-    if use_ai_launcher:
-        url = f"http://127.0.0.1:{ai_launcher_port}/v1/chat/completions"
-
-    else:
-        url = "https://mchilvers.pyscriptapps.com/my/api-proxies/openai-completions"
+    url = f"http://127.0.0.1:{ai_launcher_port}/v1/chat/completions"
 
     content = f"""
     Respond as an 18th century pirate.
