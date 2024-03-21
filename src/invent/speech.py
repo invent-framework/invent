e915be34c0eba35e95d71fa3f909b5385f6a4f37
Martin <martin.chilvers@gmail.com>
2024-03-20 20:18:14 -0500

    Implement listen using Python Future.

--- src/invent/speech.py
+++ src/invent/speech.py
@@ -1,54 +1,96 @@
-import json
 from pyscript import fetch, window
+from .compatability import proxy
+
 
 try:
     synth = window.speechSynthesis
-except:
-    # If tts is not supported we disable the action buttons
+
+except AttributeError:
     print("Sorry, your browser doesn't support text to speech!")
-    synth = None
     raise
 
 
-def say(text):
-    """Ask the overlords!"""
-    print("Saying:", text)
-    msg = window.SpeechSynthesisUtterance.new()
-    msg.text = text
-    synth.speak(msg)
-    print("Done")
+try:
+    SpeechRecognition = window.SpeechRecognition
 
+except AttributeError:
+    try:
+        SpeechRecognition = window.webkitSpeechRecognition
+
+    except AttributeError:
+        print("Sorry, your browser doesn't support speech recognition!")
+        raise
+
+
+def get_voice_by_name(voice_name):
+    """
+    Return the voice with the specified name.
+
+    Defaults to the first voice.
+    """
+
+    for voice in synth.getVoices():
+        if voice.name == voice_name:
+            break
+
+    else:
+        voice = synth.getVoices()[0]
+
+    return voice
+
+
+def say(text):
+    """
+    Say the specified text using speech synthesis.
+    """
+    utterance = window.SpeechSynthesisUtterance.new()
+    utterance.voice = get_voice_by_name("Catherine")
+    utterance.text = text
+    synth.speak(utterance)
+
+
+async def listenJS():
+    """
+    Speech recognition via the microphone using pure JS.
+    """
+    return await window.recognizeSpeech()
 
 
 async def listen():
-    return await window.recognizeSpeech()
-# window.recognizeSpeech
+    """
+    Speech recognition via the microphone.
+    """
 
-# async def listen():
-#     try:
-#         SpeechRecognition =  window.SpeechRecognition
-#     except AttributeError:
-#         try:
-#             SpeechRecognition = window.webkitSpeechRecognition
-#         except AttributeError:
-#             print("Unable to find the SpeechRecognition or webkitSpeechRecognition APIs")
+    import asyncio
 
-#     recognition = SpeechRecognition.new()
+    recognition = SpeechRecognition.new()
 
-#     def on_start(e=None):
-#         print("starting to listen... SPEAK!")
+    # The speech recognition API is non-blocking but uses callbacks, so here
+    # we wrap it with a Future.
+    future = asyncio.Future()
 
-#     def on_stop(e=None):
-#         print("stopped listening")
-#         recognition.stop()
+    def on_result(event):
+        transcript = event.results.item(0).item(0).transcript
+        future.set_result(transcript)
 
-#     def on_result(result):
-#         window.console.log(result.results)
-#         print(result.results[0][0].transcript)
+    def on_stop(event):
+        recognition.stop()
 
-#     recognition.onspeechend = on_stop
-#     recognition.onresult = on_result
-#     recognition.onstart = on_start
+    def on_error(event):
+        raise Exception(str(event))
 
-#     # We start the recognition here
-#     recognition.start();
+    recognition.onresult = proxy(on_result)
+    recognition.onstop = proxy(on_stop)
+    recognition.onerror = proxy(on_error)
+
+    # Auto-stops when it detects a pause in the user's speech.
+    recognition.continuous = False
+
+    # If the speech synthesizer is still speaking, wait for it to shut up,
+    # otherwise the microphone will pick up what it is saying :)
+    while synth.speaking:
+        await asyncio.sleep(0.1)
+
+    recognition.start()
+
+    return await future
