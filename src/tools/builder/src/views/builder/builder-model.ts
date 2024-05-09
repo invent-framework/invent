bdcac6c6a9971f6d0bc518904e6940e667d44e02
Alex Martinez <alexmtz298@gmail.com>
2024-05-09 18:39:09 -0500

    Cleanup unused code

--- src/tools/builder/src/views/builder/builder-model.ts
+++ src/tools/builder/src/views/builder/builder-model.ts
@@ -7,11 +7,9 @@ import type { WidgetModel } from "@/data/models/widget-model";
 import type { PageModel } from "@/data/models/page-model";
 import * as Blockly from 'blockly/core';
 import { pythonGenerator } from 'blockly/python';
-import confetti from "canvas-confetti";
 import type { DatastoreValueModel } from "@/data/models/datastore-value-model";
 import type { MediaFileModel } from "@/data/models/media-file-model";
 import type { IbSelectOption } from "@/components/ib-select/ib-select-types";
-import { CommonUtilities } from "@/utilities/common-utilities";
 
 
 /**
@@ -24,10 +22,6 @@ export class BuilderModel extends ViewModelBase {
 	protected getLocalizationNamespace(): string {
 		return "builder";
 	}
-
-	private apiKey = "psdc_gAAAAABl5zj-GCPoqFRv62GrpO8ud1mZhz_bbMTXSwwd1WR0ayuaiFLl15WnafvFiKMQEULC1YdSLOu4P8PEr5Cj8WPTJq2w0bgZsusOIur9UKf17tIsSlRHrDDEWLpHD1GSooHYvLNyLDFfoGDPEd50pfdoKDy8F7K3plvTjQfEC5lGnNjKt53uKlrwrEFJmLiGiV9-U4TD_uNUOAwnnIHOxMtZ0UI-MQ==";
-	private username = "joshualowe1002";
-	private projectSlug = CommonUtilities.getRandomId("invent", "").toLowerCase();
 
 	/**
 	 * Reactive instance of the view state.
@@ -43,12 +37,22 @@ export class BuilderModel extends ViewModelBase {
 			await new Promise(r => setTimeout(r, 10));
 		}
 
-	// private async setupProject(): Promise<void> {
-	// 	this.state.project = await this.getProject();
-	// 	if (this.state.project === null) {
-	// 		this.state.project = await this.createProject("Invent Demo", "app");
-	// 	}
-	// }
+		this.getPages();
+		this.setDefaultPage();
+		this.getAvailableComponents();
+		/*
+			* BuilderUtilities is really just a bridge between this class (the JS-side of
+			* the view model) and the "Builder" class in "builder.py" (the Python-side of
+			* the view model).
+			*/
+		BuilderUtilities.init(this);
+
+		this.listenForIframeMessages();
+
+		window.parent.postMessage({
+			type: "invent-ready"
+		}, location.origin);
+	}
 
 	// Pages ///////////////////////////////////////////////////////////////////////////
 
@@ -213,43 +217,6 @@ export class BuilderModel extends ViewModelBase {
 		return datastoreCode.join("\n");
 	}
 
-	public async getPythonCode(): Promise<any> {
-		this.state.isPublishing = true;
-
-		const datastore: string = this.getDatastoreValues();
-		const generatedCode: string = pythonGenerator.workspaceToCode(Blockly.getMainWorkspace());
-		const code: string = `${this.state.functions}\n${generatedCode}`;
-
-		const result: any = BuilderUtilities.exportAsPyScriptApp(datastore, code);
-		const indexHtml: string  = result["index.html"];
-		const mainPy: string  = result["main.py"];
-		const pyscriptToml: string  = result["pyscript.toml"];
-
-		console.log(indexHtml);
-		console.log(mainPy);
-		console.log(pyscriptToml);
-
-		await Promise.all([
-			this.uploadFile(this.createFormDataBlob('index.html', indexHtml, 'text/html')),
-			this.uploadFile(this.createFormDataBlob('pyscript.toml', pyscriptToml, 'application/toml')),
-			this.uploadFile(this.createFormDataBlob('main.py', mainPy, 'application/x-python-code')),
-	  	]).then(() => {
-			confetti({
-				particleCount: 100,
-				spread: 200
-			});
-
-			ModalUtilities.showModal({
-				modal: "AppPublished",
-				options: {
-					url: this.state.project.latest.url
-				}
-			});
-
-			this.state.isPublishing = false;
-		});
-	}
-
 	public async load(data: any): Promise<void> {
 		// Load App
 		BuilderUtilities.getAppFromDict(data.app);
@@ -302,89 +269,6 @@ export class BuilderModel extends ViewModelBase {
 		return formData;
 	}
 
-	public async getProject() {
-		const response = await fetch(
-			`/api/projects/${this.username}/${this.projectSlug}`,
-			{
-				method: "GET",
-				headers: {
-					"Authorization": `Bearer ${this.apiKey}`,
-				}
-			}
-		);
-		if (response.status === 404) {
-			return null;
-		}
-
-		const result = await response.json();
-		if (response.status != 200) {
-			throw result;
-		}
-
-		return result;
-	}
-
-	public async createProject(description: string, type: string) {
-		const response = await fetch(
-			`/api/projects/`,
-			{
-				method: "POST",
-				headers: {
-					"Authorization": `Bearer ${this.apiKey}`,
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					name: this.projectSlug, description, type
-				})
-			}
-		);
-
-		const result = await response.json();
-		if (response.status != 200) {
-			throw result;
-		}
-
-		return result;
-	}
-
-	public async uploadFile(formData: FormData): Promise<any> {
-		const endpoint = `/api/projects/${this.state.project.id}/files?overwrite=True`;
-		const response = await fetch(endpoint, {
-			method: 'POST',
-			body: formData,
-			headers: {
-				"Authorization": `Bearer ${this.apiKey}`,
-			},
-	  	});
-
-		const result = await response.json();
-		if (response.status != 200) {
-			throw result;
-		}
-
-		return result;
-	}
-
-	public async uploadMediaFile(file: File): Promise<string> {
-		let mediaFolder: string = "";
-
-		if (file.type.startsWith('image')){
-			mediaFolder = "images";
-		}
-		else if (file.type.startsWith('audio')){
-			mediaFolder = "sounds";
-		}
-		else if (file.type.startsWith('video')){
-			mediaFolder = "videos";
-		}
-
-		const path: string = `media/${mediaFolder}/${file.name}`;
-		const formData: FormData = this.createFormDataFromBlob(path, file);
-
-		// this.uploadFile(formData);
-		return `https://${this.username}.pyscriptapps.com/${this.projectSlug}/latest/${path}`;
-	}
-
 	public onBuilderTabClicked(tab: string) {
 		this.state.activeBuilderTab = tab;
 
@@ -420,40 +304,6 @@ export class BuilderModel extends ViewModelBase {
 		window.parent.postMessage({
 			type: "add-media-request",
 		});
-
-		// // Create file input in order to open file chooser.
-		// const fileInput: HTMLInputElement = document.createElement("input");
-		// fileInput.type = "file";
-		// fileInput.accept = ".png, .jpg, .gif, .jpeg, .mp3, .wav, .mp4, .mov";
-		//
-		// // Runs when the user chooses a file.
-		// fileInput.addEventListener("change", (event: Event) => {
-		// 	const target: HTMLInputElement = event.target as HTMLInputElement;
-		// 	const reader: FileReader = new FileReader();
-		//
-		// 	reader.addEventListener("load", async (event: ProgressEvent<FileReader>) => {
-		// 		// Parse JSON file and then open the editor with its contents.
-		// 		if (event.target && target.files) {
-		// 			const file: File = target.files[0];
-		// 			const path: string = await this.uploadMediaFile(file);
-		// 			const fileName: string = file.name;
-		// 			this.state.media[fileName] = {
-		// 				name: fileName,
-		// 				type: file.type,
-		// 				file,
-		// 				path
-		// 			}
-		// 		}
-		// 	});
-		//
-		// 	// Trigger reading the uploaded files.
-		// 	if (target.files) {
-		// 		reader.readAsDataURL(target.files[0]);
-		// 	}
-		// });
-		//
-		// // Click on the file input to open a file chooser.
-		// fileInput.click();
 	}
 
 	public getImageFiles(): Array<IbSelectOption> {
