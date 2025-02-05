1735c70a54fd6cb28497972640d56411e60f77ee
Malcolm Smith <smith@chaquo.com>
2025-02-05 18:19:53 +0000

    Remove previous implementations of Column, Row, Button, Label and TextInput

--- src/toga_invent/widgets/textinput.py
+++ src/toga_invent/widgets/textinput.py
@@ -7,16 +7,28 @@ from .base import Widget
 class TextInput(Widget):
 
     def render(self):
-        return document.createElement("input")
+        element = document.createElement("input")
+        element.addEventListener("input", create_proxy(self.input))
+        return element
+
+    def input(self, event):
+        self.interface.on_change()
 
     def get_value(self):
         return self.element.value
 
     def set_value(self, value):
         self.element.value = value
+        self.input(None)
+
+    def get_readonly(self):
+        return self.element.readOnly
 
     def set_readonly(self, value):
         self.element.readOnly = value
+
+    def get_placeholder(self):
+        return self.element.placeholder
 
     def set_placeholder(self, value):
         self.element.placeholder = value
