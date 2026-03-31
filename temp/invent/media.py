5edeb7f7d839cb7346766a224222d3dbbcabe05a
iliketocode2 <goldmanwilliam3@gmail.com>
2026-03-31 01:02:18 -0400

    styles updated to match infania

--- temp/invent/media.py
+++
@@ -1,8 +0,0 @@
-__all__=['set_media_root','get_media_root','Media']
-__root__='.'
-def set_media_root(root):global __root__;__root__=root
-def get_media_root():global __root__;return __root__
-class Media:
-	def __init__(A,path,name):A._path=path;A._name=name
-	def __getattr__(A,attr_name):return Media(A._path+[A._name],attr_name)
-	def __str__(A):global __root__;B=[__root__]+A._path;return'/'.join(B)+'.'+A._name
--- temp/invent/ui/containers/box.py
+++
@@ -1,8 +0,0 @@
-from invent.i18n import _
-from..core.container import Container
-from..core.property import ChoiceProperty
-from..core.measures import TSHIRT_SIZES,MEDIUM
-class Box(Container):
-	gap=ChoiceProperty(_('The gap between items in the container'),choices=TSHIRT_SIZES,default_value=MEDIUM,group='style')
-	def render(B):A=super().render();A.style['display']='flex';return A
-	def on_gap_changed(A):A._set_gap(A.gap,'gap')
