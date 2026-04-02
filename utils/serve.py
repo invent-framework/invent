4a697c6c67f5ad85a373135b160385c7a39aece9
iliketocode2 <goldmanwilliam3@gmail.com>
2026-04-02 10:53:22 -0400

    comment changes in serve.py

--- utils/serve.py
+++ utils/serve.py
@@ -20,10 +20,11 @@ from watchdog.events import FileSystemEventHandler
 
 class MyHTTPRequestHandler(server.SimpleHTTPRequestHandler):
     def end_headers(self):
-        self.send_header("Access-Control-Allow-Origin:", "*")
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header(
             "Cache-Control", "no-store, no-cache, must-revalidate, max-age=0"
         )
+        # Legacy headers to make sure the browser never serves stale files
         self.send_header("Pragma", "no-cache")
         self.send_header("Expires", "0")
         # self.send_header("Cross-Origin-Opener-Policy", "same-origin")
